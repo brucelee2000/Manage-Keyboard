854999181a8331bed8ee4218071ec13bf5c9436d
brucelee2000 <brucelee2000@tp.edu.tw>
2015-01-11 17:41:52 -0800

    Add 3 methods to dismiss keyboard.

--- Manage Keyboard/ViewController.swift
+++ Manage Keyboard/ViewController.swift
@@ -8,8 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var labelText: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func buttonPressed(sender: UIButton) {
+        labelText.text = textField.text
+        
+        // Method 3: call resignFirstResponder
+        textField.resignFirstResponder()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +29,18 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // Method 1: call resignFirstResponder by using "textFieldShouldReturn" in UITextFieldDelegate protocal
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    // Method 2: call endEditing in overide function "touchesBegan"
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.view.endEditing(true)
     }
 
 
